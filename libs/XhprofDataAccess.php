1cc9504aad2db2d3f4e571273f64488fc075afef
Matthew Shafer <matthewshafer@mac.com>
2011-01-17 16:35:53 -0800

    functions added to XhprodDataAccess. I'll add more as I need them for plugins.

--- libs/XhprofDataAccess.php
+++ libs/XhprofDataAccess.php
@@ -4,11 +4,75 @@ class XhprofDataAccess
 {
 
 	private $xhprofData;
+	private $batches;
+	private $currentBatch = 0;
+	private $currentRun = -1;
+	private $currentTotalRuns = 0;
 	
 	
 	public function __construct($data)
 	{
 		$this->xhprofData = $data;
+		$this->batches = count($this->xhprofData);
+	}
+	
+	public function nextBatch()
+	{
+		$ret = false;
+		if(++$this->currentBatch < $this->batches)
+		{
+			$ret = true;
+			$this->currentTotalRuns = count($this->xhprofData[$this->currentBatch]);
+			$this->currentRun = -1;
+		}
+		else
+		{
+			// just incase someone decides to call this function a bunch it shouldn't lead to the array being out of bounds
+			$this->currentBatch--;
+		}
+		
+		return $ret;
+	}
+	
+	public function haveNextRun()
+	{
+		$ret = false;
+		
+		if(++$this->currentRun < $this->currentTotalRuns)
+		{
+			$ret = true;
+		}
+		else
+		{
+			// just incase someone decides to call this function a bunch it shouldn't lead to the array being out of bounds
+			$thos->currentRun--;
+		}
+		
+		return $ret;
+	}
+	
+	public function getCurrentRunTotals()
+	{
+		$ret = null;
+		
+		if(isset($this->xhprofData[$this->currentBatch][$this->currentRun]["totals"]))
+		{
+			$ret = $this->xhprofData[$this->currentBatch][$this->currentRun]["totals"];
+		}
+		
+		return $ret;
+	}
+	
+	public function getCurrentRunOverall()
+	{
+		$ret = null;
+		
+		if(isset($this->xhprofData[$this->currentBatch][$this->currentRun]["overall"]))
+		{
+			$ret = $this->xhprofData[$this->currentBatch][$this->currentRun]["overall"];
+		}
+		
+		return $ret;
 	}
 
 }
