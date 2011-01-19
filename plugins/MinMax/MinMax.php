3b4bcc3dbc83bdf9ddc01f872f98aa9bc30d830c
Matthew Shafer <matthewshafer@mac.com>
2011-01-19 01:08:15 -0800

    fixed up the name of a plugin

--- plugins/MinMax/MinMax.php
+++ plugins/MinMax/MinMax.php
@@ -26,6 +26,11 @@ class MinMax extends BasicPlugin
 			while($this->xhprofDataAccess->haveNextRun())
 			{
 				$tmp = $this->xhprofDataAccess->getCurrentRunOverall();
+				
+				if($tmp["cpu"] == 0)
+				{
+					print_r($tmp);
+				}
 				array_push($runArr, $tmp["cpu"]);
 			}
 			
